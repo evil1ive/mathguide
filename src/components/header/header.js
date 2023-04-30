fec0312d3437304c135ada736069a991aab60db6
evil1ive <mihaf2004@gmail.com>
2023-04-30 22:00:20 +0500

    По подключал зависимости ну и иконку закинул

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import './header.scss';
+
+export const App = () => {
+  return (
+    <header>
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+    </header>
+  );
+}
+
